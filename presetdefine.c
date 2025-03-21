fa77f4248f9dfa63ea55693822fc2db5dde6c88a
Ian Martin Ajzenszmidt <iajzenszmi@gmail.com>
2025-03-21 14:25:21 +1100

    Add files via upload

---
+++ presetdefine.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define PRESET_SPEED 100
+#define PRESET_NAME "Default Device"
+
+int main() {
+    printf("Speed: %d\n", PRESET_SPEED);
+    printf("Name: %s\n", PRESET_NAME);
+    return 0;
+}
